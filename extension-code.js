53b111d5bb177c636cfece79935690352ca7ee71
scratch-craft-2 <156839189+scratch-craft-2@users.noreply.github.com>
2025-04-27 15:13:44 +0300

    Update extension-code.js

--- extension-code.js
+++ extension-code.js
@@ -1,32 +1,40 @@
+// Name: Modals
+// ID: htmlalert
+// Description: This extension adds modal dialog boxes.
+// By: scratch_craft_2 <https://scratch.mit.edu/users/scratch_craft_2/>
+// License: MPL-2.0
+
 (function (Scratch) {
     "use strict";
-    class YaGamesSDKExtension {
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("This Extension must run unsandboxed");
+  }
+
+    class modals {
       getInfo() {
-        console.log(
-          "Расширение alert() in turbowarp - создано scratch_craft_2",
-        );
         return {
           id: "htmlalert",
           name: "modals",
           color1: "#a3c0e1",
           blocks: [
             {
-              opcode: "alert",
+              opcode: "alertb",
               blockType: Scratch.BlockType.COMMAND,
-              text: "alert [text]",
+              text: "alert [textia]",
               arguments: {
-                text: {
+                textia: {
                   defaultValue: "hello!",
                   type: Scratch.ArgumentType.STRING,
                 },
             },
            },
           {
-              opcode: "Prompt",
+              opcode: "Promptb",
               blockType: Scratch.BlockType.REPORTER,
-              text: "prompt [text], default value: [classic]",
+              text: "prompt [textip], default value: [classic]",
               arguments: {
-                text: {
+                textip: {
                   defaultValue: "what's your name?",
                   type: Scratch.ArgumentType.STRING,
                 },
@@ -37,11 +45,11 @@
             },
            },
            { 
-              opcode: "Confirm",
+              opcode: "Confirmb",
               blockType: Scratch.BlockType.BOOLEAN,
-              text: "confirm [text]",
+              text: "confirm [textic]",
               arguments: {
-                text: {
+                textic: {
                   defaultValue: "Did you see that movie?",
                   type: Scratch.ArgumentType.STRING,
                 },
@@ -50,15 +58,15 @@
           ],
         };
       }
-      alert(args) {
-        alert([args.text]);
-      }
-      Prompt(args) {
-        return prompt([args.text], [args.classic]);
-      }
-      Confirm(args) {
-        return confirm([args.text]);
-      }
+alertb(args) {
+ alert([args.textia]);
+}
+Promptb(args) {
+  return prompt([args.textip], [args.classic]);
+}
+Confirmb(args) {
+  return confirm([args.textic]);
+}
     }
-    Scratch.extensions.register(new YaGamesSDKExtension());
+    Scratch.extensions.register(new modals());
   })(Scratch);
